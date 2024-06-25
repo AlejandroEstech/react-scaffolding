a085b5379e6eeb11d12342ce0700b8698f20f9c8
jandrocode <jandrocode@gmail.com>
2024-06-25 13:47:54 +0200

    Modificando estilos en los enlaces

--- src/components/nav/LateralNav.jsx
+++ src/components/nav/LateralNav.jsx
@@ -1,39 +1,58 @@
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import '../../styles/LateralNav.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TopNav from './TopNav';
 import myImage from '../../assets/react.png'
 
 function LateralNav() {
 
   const [ collapsed, setCollapsed] = useState(false)
+  const [selectedLink, setSelectedLink] = useState(null);
+  const location = useLocation();
 
   const handleCLick = () => {
     setCollapsed(collapsed => !collapsed)
   }
+
+  const handleLinkClick = (link) => {
+    setSelectedLink(link);
+};
+
+useEffect(() => {
+  const path = location.pathname;
+  if (path === '/profile') {
+      setSelectedLink('linkProfile');
+  } else if (path === '/messages') {
+      setSelectedLink('linkMessages');
+  } else if (path === '/settings') {
+      setSelectedLink('linkSettings');
+  }
+}, [location.pathname]);
 
   return (
     <>
       <TopNav isCollapsed={collapsed} />
       <span id="toggle-btn" onClick={handleCLick}>&#9776;</span> 
       <div id="sidebar" className={collapsed ? 'collapsed' : ''}>
-
-            <Link to='#' className='link'>
-              <img src={myImage} alt=''  width='10%' className='me-1 mb-1' />
-              <span>React Seed</span>
-              <hr />
+            <Link to='#'  className='link'>
+                <img src={myImage} alt=''  width='10%' className='me-1 mb-1' />
+                <span>React Seed</span>
+                <hr />
             </Link>
 
-            <Link to='/profile' className='link'>
+            <Link to='/profile' className={selectedLink === 'linkProfile' ? 'selectedLink' : 'link'}
+                onClick={() => handleLinkClick('linkProfile')}>
               <i className='bx bxs-user me-1'></i>Perfil
             </Link>
 
-            <Link to='/messages' className='link'>
+            <Link to='/messages' className={selectedLink === 'linkMessages' ? 'selectedLink' : 'link'}
+                onClick={() => handleLinkClick('linkMessages')}>
               <i className='bx bxs-chat me-1' ></i>Mensajería
             </Link>
 
-            <Link to='/settings' className='link'>
+            <Link to='/settings' className={selectedLink === 'linkSettings' ? 'selectedLink' : 'link'}
+                onClick={() => handleLinkClick('linkSettings')}>
               <i className='bx bx-cog me-1'></i>Configuración
             </Link>
         </div>
--- src/components/nav/TopNav.jsx
+++ src/components/nav/TopNav.jsx
@@ -1,40 +1,71 @@
-import React, { useEffect, useState } from 'react'
-import '../../styles/TopNav.css'
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import '../../styles/TopNav.css';
+import { Link, useLocation } from 'react-router-dom';
 import { getUser } from '../../services/auth.service';
 
-
-function TopNav({isCollapsed}) {
-
-    const [user,setUser] = useState(false)
+function TopNav({ isCollapsed }) {
+    const [user, setUser] = useState(false);
+    const [selectedLink, setSelectedLink] = useState(null);
+    const location = useLocation();
 
     useEffect(() => {
-        setUser(getUser())
-    },[])
+        setUser(getUser());
+    }, []);
 
-        return (
-            <>
-                <nav className='top_navbar'>
-                    <ul>
-                        <li>
-                           <Link to='/dashboard' className='link'>Home</Link>
-                        </li>
-                        <li>
-                           <Link to='/us' className='link'>Nosotros</Link>
-                        </li>
-                        <li>
-                           <Link to='/contact' className='link'>Contacto</Link>
-                        </li>
-                    </ul>
-                 
-                </nav>
+    useEffect(() => {
+        const path = location.pathname;
+        if (path === '/dashboard') {
+            setSelectedLink('linkDashboard');
+        } else if (path === '/us') {
+            setSelectedLink('linkUs');
+        } else if (path === '/contact') {
+            setSelectedLink('linkContact');
+        }
+    }, [location.pathname]);
 
-                <span className='login_logout_container btn d-flex'>
-                    <i class='bx bx-exit mt-1 me-1' ></i>  {user && <p>Salir</p>}
-                </span>
+    const handleLinkClick = (link) => {
+        setSelectedLink(link);
+    };
 
-            </>
-        )
+    return (
+        <>
+            <nav className='top_navbar'>
+                <ul>
+                    <li>
+                        <Link
+                            to='/dashboard'
+                            className={selectedLink === 'linkDashboard' ? 'selectedLink' : 'link'}
+                            onClick={() => handleLinkClick('linkDashboard')}
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='/us'
+                            className={selectedLink === 'linkUs' ? 'selectedLink' : 'link'}
+                            onClick={() => handleLinkClick('linkUs')}
+                        >
+                            Nosotros
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            to='/contact'
+                            className={selectedLink === 'linkContact' ? 'selectedLink' : 'link'}
+                            onClick={() => handleLinkClick('linkContact')}
+                        >
+                            Contacto
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+
+            <span className='login_logout_container btn d-flex'>
+                <i className='bx bx-exit mt-1 me-1'></i> {user && <p>Salir</p>}
+            </span>
+        </>
+    );
 }
 
-export default TopNav
+export default TopNav;
